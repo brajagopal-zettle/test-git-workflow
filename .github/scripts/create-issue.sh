c3bd4148f2fef9b40fde0a9d5a3487cdfb46b97f
Balaji Rajagopal <brajagopal@paypal.com>
2022-12-05 17:37:21 +0100

    Changing to script

--- .github/scripts/create-issue.sh
+++ .github/scripts/create-issue.sh
@@ -1,6 +1,92 @@
-curl \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
-  https://api.github.com/repos/brajagopal/test-git-workflow/issues \
-  -d '{"title":"Found a bug","body":"I'\''m having a problem with this.","assignees":["octocat"],"milestone":1,"labels":["bug"]}'
+#!/bin/bash
+
+export MAIN_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+export RELEASE_TAG_PREFIX="release"
+export RELEASE_TITLE_PREFIX="[deploy] Release"
+export LAST_ISSUE="[Release]"
+
+# Get the latest from default branch
+# ----------------------------------
+getLatest() {
+    git checkout "$MAIN_BRANCH"
+    git pull origin "$MAIN_BRANCH"
+    git fetch
+}
+
+# Constructs the changelog. These are all commits from the latest release
+# the current going-to-be deploy SHA.
+# -----------------------------------
+getChangeLogSinceLatestRelease() {
+  latest_release_hash=$(gh api repos/iZettle/"$PROJECT_REPONAME"/releases/latest | jq -r '.target_commitish')
+
+  if [ -z "$latest_release_hash" ] || [ "$latest_release_hash" = "null" ]; then
+    # First release, empty changelog
+    echo ""
+  else
+    changelog=$(git --no-pager log \
+                --pretty='* %h %at %an %ad - %s' \
+                --no-merges \
+                --author-date-order \
+                --date=format:'%Y-%m-%d:%H:%M:%S' \
+                "$latest_release_hash" | \
+                sort -k2,1 --stable)
+    echo "$changelog"
+  fi
+}
+
+# The createDraftRelease will add content to the Issue explain what the issue
+# is about. It will add the changelog and link it to the git SHA.
+# If an issue with open already exists, it will delete it
+# and create a new one.
+# ------------------------------------------------------------------------------
+createDraftRelease() {
+    echo "Creating Issue."
+    echo "-----------------------"
+    changelog=$1
+
+    # Show all commits in the changelog if the number of commits is less than 10
+    # Otherwise show only the most recent.
+    # If it's less than `num_recent`, then no need to post a PR comment for
+    # the changelog.
+    total_commits=$(echo -n "$changelog" | wc -l)
+    num_recent=20
+    changelog_summary_title="($num_recent most recent commits)"
+    if [ "$total_commits" -eq "0" ] && [ "x$changelog" = "x" ]; then
+        changelog_summary_title="(No commits, maybe it's the first time)"
+        changelog_recent=$changelog
+    elif [ "$total_commits" -le "$num_recent" ]; then
+        changelog_summary_title="(all commits)"
+        changelog_recent=$changelog
+    else
+        changelog_recent=$(echo "$changelog" | tail -$num_recent)
+    fi
+
+    # Get the current draft release tag and delete them all.
+    current_issue=$(gh api repos/iZettle/"$PROJECT_REPONAME"/issues | jq -r "[ .[] | select( .name | contains(\"$LAST_ISSUE\")) | .[].tag_name")
+
+    # Delete all the draft releases
+    if [[ -n $current_issue ]]; then
+        printf '%s\n' "$current_issue" |
+        while IFS= read -r tag; do
+            echo "Deleting draft release with tag=$tag"
+            gh release delete -y "$tag"
+        done
+    fi
+
+
+    release_body=$(getReleaseBody)
+    gh issue create --title "$LAST_ISSUE v$(date +%Y%m%d%H%M)" \
+                    --body "$release_body"
+                    --repo $PROJECT_REPONAME
+}
+
+
+# Main entry point
+# ----------------
+# validate
+getLatest
+set -x
+changelog=$(getChangeLogSinceLatestRelease)
+createIssue "$changelog"
+set +x
+echo "Done!"
