a46fc70823061199d89f255c8134e74da3938524
Balaji Rajagopal <brajagopal@paypal.com>
2022-12-05 14:33:19 +0100

    Changing the token

--- .github/scripts/create-issue.sh
+++ .github/scripts/create-issue.sh
@@ -1,6 +1,6 @@
 curl \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer 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" \
   https://api.github.com/repos/brajagopal/test-git-workflow/issues \
   -d '{"title":"Found a bug","body":"I'\''m having a problem with this.","assignees":["octocat"],"milestone":1,"labels":["bug"]}'
